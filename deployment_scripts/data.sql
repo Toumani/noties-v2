a6a91bca6e1929297218d56d6cdd913b11dfd312
Toumani <toumani49@gmail.com>
2022-06-22 22:05:53 +0100

    devops: delete deployment scripts

--- deployment_scripts/data.sql
+++
@@ -1,4 +0,0 @@
--- noinspection SqlWithoutWhere
-update note n set
-                  nb_done = (select count(*) from todo t where t.done = true and t.note_id = n.id),
-                  nb_total = (select count(*) from todo t where t.note_id = n.id);
